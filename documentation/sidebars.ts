f344a5ee7a0c3174d1ece704a2bb1638fd4b5d58
hhertout <hugues.hertout@gmail.com>
2026-05-12 20:08:29 +0200

    docs: update the name of an item in the sidebar

--- documentation/sidebars.ts
+++ documentation/sidebars.ts
@@ -37,7 +37,7 @@ const sidebars: SidebarsConfig = {
     },
     {
       type: 'category',
-      label: 'Concepts',
+      label: 'Reference',
       items: [
         'concepts/matchers',
         'concepts/scheduling',
